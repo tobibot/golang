f2424752c66e8368ab504c754c62a35d4b6a370e
tobibot <tobias.heying@outlook.com>
2020-09-23 21:47:49 +0200

    webservice readme

---
+++ webservice/README.md
@@ -0,0 +1,4 @@
+# From a pluralsight course
+
+this is not my work. please visit https://app.pluralsight.com/library/courses/getting-started-with-go/table-of-contents for further information
+ 
--- webservice/main.go
+++ webservice/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"go.heying.info/learning/webservice/controllers"
 )
 
+const (
+	port = "3000"
+)
+
 func main() {
+	sport := fmt.Sprintf(":%v", port)
+
+	println("listening on port", port)
+
 	controllers.RegisterControllers()
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(sport, nil)
 }
