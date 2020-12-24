1f6cd430f16bda0867652ecc52f6871dbd946626
tobibot <tobias.heying@outlook.com>
2020-12-24 11:48:44 +0100

    Recover

--- learninggo/recover_test.go
+++ learninggo/recover_test.go
@@ -1,8 +1,22 @@
 package learninggo
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRecover(t *testing.T) {
 
-	// Todo
+	defer recoverFromPanic()
+
+	fmt.Println("doing something")
+	panic("ooops")
+
+	fmt.Println("Finisch")
+}
+
+func recoverFromPanic() {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+	}
 }
